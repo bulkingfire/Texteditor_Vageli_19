6aecab5885a1edee753fa5f1823fafe9987c5e81
bulkingfire <118399104+bulkingfire@users.noreply.github.com>
2023-05-17 19:19:38 -0500

    Add files via upload

---
+++ client/webpack.config.js
@@ -0,0 +1,49 @@
+const plugin1 = require('html-webpack-plugin');
+const web1 = require('webpack-pwa-manifest');
+const route2 = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: 
+      install: 
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: route2.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new plugin1({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
